b3fb1b1f4f405c8a3114f16b027c33b6f90b7d75
LuisMZampar <rm550531@fiap.com.br>
2023-09-13 22:56:15 -0300

    Exemplos de useEffect

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -3,16 +3,34 @@ import { ListaProdutos } from "../components/ListaProdutos";
 import { AiFillEdit as Editar } from "react-icons/ai";
 import { RiDeleteBin2Fill as Excluir } from "react-icons/ri";
 import styles from "./Produtos.module.css";
+import { useState, useEffect } from "react";
 
 export default function Produtos() {
-
   document.title = "Produtos";
+
+  const [count, setCount] = useState(0);
+
+  //Neste formato o useEffect executa sempre que ocorrer uma alteração de estado de algum elemento ou no componente.
+  // useEffect(() => {
+  //   console.log(`Executa sempre - ${count} !`);
+  // });
+
+//Neste formato o useEffect executa apenas quando ocorrer o carregamento do componente rprincipal.
+  useEffect(() => {
+    console.log("Executa apenas uma vez");
+  },[]);
+  
+  useEffect(() => {
+    console.log("Executa sempre que ocorrer uma alteração de estado do elemento ou no componente indicado no array de c");
+  },[count]);
 
 
   return (
     <>
       <h1>Lista de Produtos</h1>
-
+      <div>
+        <button onClick={()=> setCount( count + 1)}>COUNTER - {count}</button>
+      </div>
       <div>
         <table className={styles.tblEstilo}>
 
@@ -28,15 +46,27 @@ export default function Produtos() {
           </thead>
 
           <tbody>
-
             {ListaProdutos.map((item, indice) => (
               <tr key={indice} className={styles.tblRow}>
                 <td>{item.id}</td>
                 <td>{item.nome}</td>
                 <td>{item.desc}</td>
-                <td><img src={`${item.img}`} alt={`${item.desc}`} /></td>
+                <td>
+                  <img src={`${item.img}`} alt={`${item.desc}`} />
+                </td>
                 <td>{item.preco}</td>
-                <td> <Link to={`/editar/produtos/${item.id}`}> <Editar /> </Link> / <Link to={`/excluir/produtos/${item.id}`}> <Excluir /> </Link></td>
+                <td>
+                  {" "}
+                  <Link to={`/editar/produtos/${item.id}`}>
+                    {" "}
+                    <Editar />{" "}
+                  </Link>{" "}
+                  /{" "}
+                  <Link to={`/excluir/produtos/${item.id}`}>
+                    {" "}
+                    <Excluir />{" "}
+                  </Link>
+                </td>
               </tr>
             ))}
           </tbody>
@@ -46,10 +76,9 @@ export default function Produtos() {
               <td colSpan={6}>PRODUTOS</td>
             </tr>
           </tfoot>
+
         </table>
-
       </div>
-
     </>
-  )
+  );
 }
